d567a5b40ae5edbad5a6e474d0afcdc9c29f4ed3
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-09-13 12:50:57 +0700

    05 recursice

--- optional-05-recursive/fibonacci.js
+++ optional-05-recursive/fibonacci.js
@@ -1,5 +1,16 @@
 function fibonacci(n) {
+    const fibo = (n) => {
+        if (n <= 1) {
+            return n;
+        }
+        return fibo(n-1) + fibo(n-2);
+    };
 
+    let result = [];
+    for (let i = 0; i <= n; i++) {
+        result.push(fibo(i));
+    }
+    return result;
 }
 
 // Jangan hapus kode di bawah ini!
